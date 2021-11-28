122fe8be9137180acf8a738e4c5f32aaf225b2bc
mehmet <ilhanmehmet40@gmail.com>
2021-11-28 14:34:31 +0300

    initial commit

---
+++ src/main/java/com/school/SchoolApplication.java
@@ -0,0 +1,14 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
